208456b845e39a10f8f6095c5c1e68a01ea1808d
Meng Dong <mengdong03@hotmail.com>
2015-11-07 21:41:15 -0500

    routes

--- FlaskApp/app/routes.py
+++ FlaskApp/app/routes.py
@@ -1,18 +1,11 @@
 from flask import Flask, render_template
 from flask.ext.pymongo import PyMongo
 from flask import jsonify
-<<<<<<< HEAD
 import db
  
 app = Flask(__name__)  
-mongo = PyMongo(app)   
-
-=======
+mongo = PyMongo(app)      
  
-app = Flask(__name__)  
-mongo = PyMongo(app)    
- 
->>>>>>> origin/master
 @app.route('/')
 def home():
   return render_template('cover_page.html')
@@ -21,17 +14,10 @@ def home():
 def dash():
   return render_template('dash.html')
 
-<<<<<<< HEAD
 @app.route("/time/<name>/<time_from>/<time_to>")
 def save_time_by_user(name, time_from, time_to):
   print("hw");
   return jsonify({ 'name': name, 'from': time_from, 'to': time_to})
-=======
-@app.route("/time/<time>/<user>")
-def save_time_by_user(time, user):
-  print("hw");
-  return jsonify({'time': time, 'user': user})
->>>>>>> origin/master
  
 if __name__ == '__main__':
   app.run(debug=True)
